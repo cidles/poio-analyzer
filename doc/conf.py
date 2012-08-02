ed0cd85391169f5936a0df1c6fac08f36e9ddec2
Peter Bouda <pbouda@cidles.eu>
2012-08-02 12:38:37 +0100

    Changed copyright, removed sphinxtogithubpages dependency.

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,7 @@ import sys, os
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage',
-              'sphinx.ext.viewcode', 'numpydoc', 'sphinxtogithub']
+              'sphinx.ext.viewcode', 'numpydoc' ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Poio'
-copyright = u'2012, Peter Bouda'
+copyright = u'2012, Centro Interdisciplinar de Documentação Linguística e Social'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
